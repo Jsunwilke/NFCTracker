cd4cf35c85a95ed24d7ef42885e1dd10f2ee90fc
Jason Wilkey <jsunwilke@gmail.com>
2025-03-01 20:25:26 -0500

    Initial Commit

---
+++ NFC Sd TrackerTests/NFC_Sd_TrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  NFC_Sd_TrackerTests.swift
+//  NFC Sd TrackerTests
+//
+//  Created by administrator on 3/1/25.
+//
+
+import Testing
+@testable import NFC_Sd_Tracker
+
+struct NFC_Sd_TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
